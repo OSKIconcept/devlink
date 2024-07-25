e199ac512e1b82ad52d11f72657e61ed2266c568
oskiconcept <oscarjoseph7991@gmail.com>
2024-07-25 13:16:31 +0100

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    apiKey: "AIzaSyAJAVszWy-CEk_lYAPP68zjI3dKQUXmFDU",
-    authDomain: "my-devlink.firebaseapp.com",
-    projectId: "my-devlink",
-    storageBucket: "my-devlink.appspot.com",
-    messagingSenderId: "1085388708565",
-    appId: "1:1085388708565:web:f2c976d244600fc510e05c",
-  },
+  // env: {
+  //   NEXT_PUBLIC_FIREBASE_API_KEY=AIzaSyAJAVszWy-CEk_lYAPP68zjI3dKQUXmFDU,
+  //   NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=my-devlink.firebaseapp.com,
+  //    NEXT_PUBLIC_FIREBASE_PROJECT_ID=my-devlink,
+  //  NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET=my-devlink.appspot.com,
+  //   NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=1085388708565,
+  //    NEXT_PUBLIC_FIREBASE_API_ID=1:1085388708565:web:f2c976d244600fc510e05c
+  // },
 };
 
 export default nextConfig;
